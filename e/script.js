f0b910eac374e67a66b1264e7cefccff6954b786
IbnuParvez <parvezfatma98@gmail.com>
2025-10-21 16:19:45 +0300

    edited both js and html

--- e/script.js
+++ e/script.js
@@ -1,16 +1,21 @@
 function checkAge(){
-let age = Number(prompt("What is your age?"));
+let age = Number(document.getElementById("guestAge").value);
+const display = document.getElementById("displayAge");
 if (age >= 18){
- 	console.log(`You are an adult as your age is ${age}`);
+ 	display.textContent(`You are an adult as your age is ${age}`);
 } else {
-	console.log("You are still a child sorry!");
+	display.textContent("You are still a child sorry!");
 	}
 }
 
 const checkName = ()  => {
-	let name = prompt("What is your name");
-	console.log(`You are called ${name}`);
+	let name = (document.getElementById("guestName").value);
+	const display = document.getElementById("displayName");
+	display.textContent(`You are called ${name}`);
 }
+
+document.getElementById("nameBtn".addEventListener("click", checkName);
+document.getElementById("ageBtn".addEventListener("click", checkAge);
 
 const list = document.getElementById("topics");
 
