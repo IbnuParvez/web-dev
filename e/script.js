3370ac95f89e6c71ffcfa4c003b0f36eab499357
IbnuParvez <parvezfatma98@gmail.com>
2025-10-22 11:59:14 +0300

    updated script paranthesis mistake

--- e/script.js
+++ e/script.js
@@ -2,20 +2,20 @@ function checkAge(){
 let age = Number(document.getElementById("guestAge").value);
 const display = document.getElementById("displayAge");
 if (age >= 18){
- 	display.textContent(`You are an adult as your age is ${age}`);
+ 	display.textContent = `You are an adult as your age is ${age}`;
 } else {
-	display.textContent("You are still a child sorry!");
+	display.textContent = "You are still a child sorry!";
 	}
 }
 
 const checkName = ()  => {
-	let name = (document.getElementById("guestName").value);
+	let name = document.getElementById("guestName").value;
 	const display = document.getElementById("displayName");
-	display.textContent(`You are called ${name}`);
+	display.textContent = `You are called ${name}`;
 }
 
-document.getElementById("nameBtn".addEventListener("click", checkName);
-document.getElementById("ageBtn".addEventListener("click", checkAge);
+document.getElementById("nameBtn").addEventListener("click", checkName);
+document.getElementById("ageBtn").addEventListener("click", checkAge);
 
 const list = document.getElementById("topics");
 
